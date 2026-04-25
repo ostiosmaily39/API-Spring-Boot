f9466ef06588500df27dc61559246a777ff37691
allison catalina <allisoncatalina30@gmail.com>
2026-04-24 20:16:14 -0500

    ingreso de usuarios

---
+++ api-spring-boot/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (nombre, email, edad) VALUES ('Alice', 'alice@example.com', 25);
+INSERT INTO users (nombre, email, edad) VALUES ('Bob', 'bob@example.com', 30);
+INSERT INTO users (nombre, email, edad) VALUES ('Carlos', 'carlos@example.com', 22);
+INSERT INTO users (nombre, email, edad) VALUES ('Diana', 'diana@example.com', 28);
